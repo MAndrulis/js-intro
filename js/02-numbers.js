f0c836515e00e5f8c3eafd91a58d29aa2db993b2
Modestas And <modestas.andrulis@gmail.com>
2023-05-29 15:06:24 +0200

    Numbers

---
+++ js/02-numbers.js
@@ -0,0 +1,86 @@
+console.clear ();
+
+/*NUMBERS
+
+Sveikieji/desimtainiai
+Teigiami/neigiami
+Mistiniai: Begalybes ir NaN (not a number)
+
+Aritmetiniai operatoriai:
++, -, /, *
+
+Salia matematikos:
+(, )
+
+Kintamuju iniciavimo budai:
+const - default
+let - naudoti, tik jei reikia keisti reiksmes
+var - senove, todel niekada nenauduoti, nebent TIKRAI ZINAI KA DARAI.
+
+*/
+
+console.log (1);
+console.log (2);
+console.log (3,14);
+console.log (2.72);
+
+console.log (-1);
+console.log (-2);
+console.log (-3,14);
+console.log (-2.72);
+
+console.log (Infinity);
+console.log (-Infinity);
+
+console.log (NaN);
+
+console.log (2 + 2, 4);
+console.log (2 + 2 * 2, 6);
+console.log ((2+2) * 2, 8); /*8 atsakymo atspejimas*/
+
+console.log (0.1 + 0.2, 0,3);
+
+console.clear ()
+
+//KINTAMIEJI
+
+const amzius = 20;
+console.log (amzius)
+
+/** const saldainiuKiekis = 19; 
+ const saldainiu_kiekis = 19;
+ const SALDAINIU_KIEKIS = 19;
+*/
+const saldainiuKiekis = 19;
+console.log (saldainiuKiekis)
+
+console.clear()
+//KINTAMIEJI: konstantos ir kintamieji
+
+let pinige = 0;
+console.log (pinige);
+
+pinige = 5;
+console.log(pinige);
+console.log(pinige);
+
+pinige = 20;
+console.log(pinige);
+console.log(pinige);
+
+pinige = 4 + 3;
+console.log(pinige)
+
+pinige = pinige +3;
+console.log (pinige);
+
+pinige = pinige *4;
+console.log(pinige);
+
+
+
+
+
+
+
+
