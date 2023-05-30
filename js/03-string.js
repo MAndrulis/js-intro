918bd38121bcfda7c1b015155205890bdcd3fab2
Modestas And <modestas.andrulis@gmail.com>
2023-05-30 13:46:22 +0200

    string

---
+++ js/03-string.js
@@ -0,0 +1,29 @@
+console.clear ();
+
+/*
+STRING - tekstiniu simboliu grandinele, aka tekstas
+
+Iniciavimo budai:
+- viengubos kabutes ()
+- dvigubos kabutes ("")
+*/
+
+const vardas = 'Jonas';
+const vardas2 = "Maryte";
+
+console.log(vardas);
+console.log(vardas2);
+
+const firstName = 'Ona';
+const lasName = 'Onaityte';
+
+//Ona Onaityte
+const fullName = firstName + ' '  + lasName;
+console.log(fullName);
+
+//Sveiki, as esu Ona!
+//Sveiki, as esu Jonas!
+//Sveiki, as esu Maryte!
+const rekiam = 'Sveiki, as esu ' + firstName + '!' ;
+
+console.log(rekiam)
