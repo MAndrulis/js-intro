9727e1857cfa6f51451b0e67d8aee5b9037b8587
Modestas And <modestas.andrulis@gmail.com>
2023-05-31 16:14:50 +0200

    Update

---
+++ js/04-boolean.js
@@ -0,0 +1,30 @@
+console.clear ();
+/*BOOLEAN - logine reiksme
+true,false
+
+Loginiai operatoriai:
+&& (and) - ir (true --> jeigu visi yra true)
+|| (or) - arba  (true --> jeigu bent vienas yra true)
+
+*/
+
+const isOld = true;
+console.log(isOld);
+
+console.log('-------------');
+
+const isFunny = false;
+console.log(isFunny);
+
+console.log(true && true);
+console.log(true && flase);
+console.log(false && flase);
+console.log(false && true);
+
+console.log('-------------');
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+console.log(false || true);
+
