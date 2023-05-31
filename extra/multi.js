e6785feecb5d933425c963cb9a0934b0b1ec895a
Modestas And <modestas.andrulis@gmail.com>
2023-05-31 15:00:45 +0200

    import export

---
+++ extra/multi.js
@@ -0,0 +1,19 @@
+const multiplier = 3;
+let number = 0;
+
+const lentele = `
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number}
+ ${multiplier} * ${++number} = ${multiplier * number} `;
+
+ export { lentele, 
+    multiplier, 
+    number,
+ };
---
+++ js/05-import.js
@@ -0,0 +1,16 @@
+import count from '../extra/number.js';
+import str from '../extra/string.js';
+import { lentele as table, number as n, multiplier } from '../extra/multi.js';
+
+const lentele = 'LENTELE - NELIESTI MANES!!!';
+const number = 777;
+
+console.clear ();
+
+console.log(count);
+console.log(str);
+console.log(lentele);
+console.log(number);
+console.log(multiplier);
+console.log(table);
+console.log(n);
